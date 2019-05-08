0f6cf3732c053a003847f842bccc5fe9f1a8b81c
Gabriel SULLICE <gabriel@sullice.com>
2019-05-08 09:18:46 -0600

    add a hypermedia provider to JSON:API Commments

--- modules/jsonapi_comments/src/Controller/JsonapiCommentsController.php
+++ modules/jsonapi_comments/src/Controller/JsonapiCommentsController.php
@@ -32,30 +32,13 @@ class JsonapiCommentsController extends EntityResource {
       throw $resource_object;
     }
     $internal_field_name = $request->get(Routes::COMMENT_FIELD_NAME_KEY);
-    $public_field_name = $resource_type->getPublicName($internal_field_name);
     $comment_storage = $this->entityTypeManager->getStorage('comment');
     assert($comment_storage instanceof CommentStorageInterface);
     // @todo: add actual support for the `page` parameter.
     $pagination = $this->getPagination($request, $resource_type);
     $comments = $comment_storage->loadThread($entity, $internal_field_name, CommentManagerInterface::COMMENT_MODE_FLAT);
-    $resource_objects = array_map(function (CommentInterface $comment) use ($resource_type, $entity, $public_field_name) {
-      $resource_object = $this->entityAccessChecker->getAccessCheckedResourceObject($comment);
-      $reply_url = Url::fromRoute(
-        "jsonapi.{$resource_type->getTypeName()}.jsonapi_comments.{$public_field_name}.child_reply",
-        ['entity' => $entity->uuid(), 'parent' => $comment->uuid()]
-      );
-      $cacheability = CacheableMetadata::createFromObject($entity)->addCacheableDependency($comment);
-      $link_relations = ['https://jsonapi.org/profiles/drupal/hypermedia/#add'];
-      $link_attributes = ['linkParams' => ['rel' => $link_relations]];
-      // All of this is just copied so that the `reply` link can be added.
-      return new ResourceObject(
-        $resource_object,
-        $resource_object->getResourceType(),
-        $resource_object->getId(),
-        $comment->getLoadedRevisionId(),
-        $resource_object->getFields(),
-        $resource_object->getLinks()->withLink('reply', new Link($cacheability, $reply_url, $link_relations, $link_attributes))
-      );
+    $resource_objects = array_map(function (CommentInterface $comment) {
+      return $this->entityAccessChecker->getAccessCheckedResourceObject($comment);
     }, $comments);
     $primary_data = new ResourceObjectData($resource_objects);
     $response = $this->respondWithCollection($primary_data, $this->getIncludes($request, $primary_data), $request, $resource_type, $pagination);
--- modules/jsonapi_comments/src/HypermediaProvider/DefaultProvider.php
+++ modules/jsonapi_comments/src/HypermediaProvider/DefaultProvider.php
@@ -5,49 +5,92 @@ namespace Drupal\jsonapi_comments\HypermediaProvider;
 use Drupal\comment\CommentFieldItemList;
 use Drupal\comment\Plugin\Field\FieldType\CommentItemInterface;
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Url;
 use Drupal\jsonapi\JsonApiResource\Link;
 use Drupal\jsonapi\JsonApiResource\LinkCollection;
 use Drupal\jsonapi\JsonApiResource\ResourceObject;
+use Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface;
 use Drupal\jsonapi_hypermedia\HypermediaProviderInterface;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class DefaultProvider implements HypermediaProviderInterface {
+
+  /**
+   * @var \Drupal\jsonapi\ResourceType\ResourceTypeRepositoryInterface
+   */
+  protected $resourceTypeRepository;
+
+  public function __construct(ResourceTypeRepositoryInterface $resource_type_repository) {
+    $this->resourceTypeRepository = $resource_type_repository;
+  }
 
   public function hyperlink(LinkCollection $link_collection) {
     $context = $link_collection->getContext();
     if (!$context instanceof ResourceObject) {
       return $link_collection;
     }
-    $resource_type = $context->getResourceType();
+    // Add a `reply` link If the links is for a comment resource object.
+    if ($context->getResourceType()->getEntityTypeId() === 'comment') {
+      $link_collection = $this->addCommentReplyLink($context, $link_collection);
+    }
+    // Add a link to this module's comment collection for any comment fields.
     foreach ($context->getFields() as $field) {
       if (!$field instanceof CommentFieldItemList) {
         continue;
       }
-      $comment_field_name = $field->getName();
-      $comment_route_name = "jsonapi.{$resource_type->getTypeName()}.jsonapi_comments.{$comment_field_name}";
-      try {
-        $comments_url = Url::fromRoute($comment_route_name, ['entity' => $context->getId()]);
-      }
-      catch (RouteNotFoundException $e) {
-        continue;
-      }
-      $link_relations = [];
-      if ($field->status != CommentItemInterface::HIDDEN) {
-        $link_relations[] = 'collection';
-      }
-      if ($field->status == CommentItemInterface::OPEN) {
-        $link_relations[] = 'https://jsonapi.org/profiles/drupal/hypermedia/#add';
-      }
-      $link_attributes = [
-        'linkParams' => ['rel' => $link_relations],
-        'commentFieldName' => $resource_type->getPublicName($comment_field_name),
-      ];
-      if (!empty($link_relations)) {
-        $cacheability = CacheableMetadata::createFromObject($context);
-        $link = new Link($cacheability, $comments_url, $link_relations, $link_attributes);
-        $link_collection = $link_collection->withLink('comments', $link);
-      }
+      $link_collection = $this->addCommentCollectionLink($context, $field, $link_collection);
+    }
+    return $link_collection;
+  }
+
+  protected function addCommentReplyLink(ResourceObject $comment_object, LinkCollection $link_collection) {
+    $host_entity = $comment_object->getField('entity_id')->entity;
+    assert($host_entity instanceof FieldableEntityInterface);
+    $field_name = $comment_object->getField('field_name')->value;
+    if (!$host_entity->hasField($field_name) || $host_entity->{$field_name}->status != CommentItemInterface::OPEN) {
+      return $link_collection;
+    }
+    $public_field_name = $comment_object->getResourceType()->getPublicName($field_name);
+    $host_resource_type = $this->resourceTypeRepository->get($host_entity->getEntityTypeId(), $host_entity->bundle());
+    $reply_route_name = "jsonapi.{$host_resource_type->getTypeName()}.jsonapi_comments.{$public_field_name}.child_reply";
+    $reply_route_parameters = [
+      'entity' => $host_entity->uuid(),
+      'parent' => $comment_object->getId(),
+    ];
+    $reply_url = Url::fromRoute($reply_route_name, $reply_route_parameters);
+    $cacheability = CacheableMetadata::createFromObject($host_entity)->addCacheableDependency($comment_object);
+    $link_relations = ['https://jsonapi.org/profiles/drupal/hypermedia/#add'];
+    $link_attributes = ['linkParams' => ['rel' => $link_relations]];
+    $link = new Link($cacheability, $reply_url, $link_relations, $link_attributes);
+    return $link_collection->withLink('reply', $link);
+  }
+
+  protected function addCommentCollectionLink(ResourceObject $context, CommentFieldItemList $field, LinkCollection $link_collection) {
+    $resource_type = $context->getResourceType();
+    $comment_field_name = $field->getName();
+    $comment_route_name = "jsonapi.{$resource_type->getTypeName()}.jsonapi_comments.{$comment_field_name}";
+    try {
+      $comments_url = Url::fromRoute($comment_route_name, ['entity' => $context->getId()]);
+    }
+    catch (RouteNotFoundException $e) {
+      return $link_collection;
+    }
+    $link_relations = [];
+    if ($field->status != CommentItemInterface::HIDDEN) {
+      $link_relations[] = 'collection';
+    }
+    if ($field->status == CommentItemInterface::OPEN) {
+      $link_relations[] = 'https://jsonapi.org/profiles/drupal/hypermedia/#add';
+    }
+    $link_attributes = [
+      'linkParams' => ['rel' => $link_relations],
+      'commentFieldName' => $resource_type->getPublicName($comment_field_name),
+    ];
+    if (!empty($link_relations)) {
+      $cacheability = CacheableMetadata::createFromObject($context);
+      $link = new Link($cacheability, $comments_url, $link_relations, $link_attributes);
+      return $link_collection->withLink('comments', $link);
     }
     return $link_collection;
   }
